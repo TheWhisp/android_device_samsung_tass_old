2d021d30c818600ff3992316e8df89476ad9e599
Dāvis Mālnieks <daavis.90@gmail.com>
2012-08-24 21:23:58 +0300

    Revert "clean-up"
    This reverts commit 042d9794ffad50062499ffcfcc748548d15e7238.

--- cm.mk
+++ cm.mk
@@ -1,12 +1,11 @@
-## Device specific configuration
-$(call inherit-product, device/samsung/tass/tass.mk)
-
-## CyanogenMod stuff
+## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
-## AOSP stuff
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit device configuration
+$(call inherit-product, device/samsung/tass/full_tass.mk)
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := GalaxyMini
